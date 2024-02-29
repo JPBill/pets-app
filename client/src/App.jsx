97b95eb87592ebbd4c5afb780446bf0aeac6d75c
Juan Pablo Bill <juampibill1995@gmail.com>
2024-02-29 10:03:11 -0300

    create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,24 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Profile from './pages/Profile';
+import NotFound from './pages/NotFound';
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/iniciar-sesion" element={<Login />} />
+          <Route path="/crear-cuenta" element={<Register />} />
+          <Route path="/mi-perfil" element={<Profile />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
