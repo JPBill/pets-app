f512832c8bb9dc38f84872369327ce2aeee686b7
Juan Pablo Bill <juampibill1995@gmail.com>
2024-02-29 11:32:05 -0300

    create a test api route

--- server/index.js
+++ server/index.js
@@ -4,9 +4,13 @@ import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 dotenv.config();
 
+import userRouter from './routes/userRoute.js';
+
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
+
+app.use('/server/user', userRouter);
 
 mongoose
   .connect(process.env.MONGO)
